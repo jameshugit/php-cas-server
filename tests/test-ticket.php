3974cfa85a6e89996920582727df20d3392b703d
Pgl <pgl@erasme.org>
2011-01-10 12:35:52 +0100

    Ajout rep de tests unitaire + test-ticket.php

---
+++ tests/test-ticket.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Test de la classe de gŽnŽration de tickets.
+ */
+include('../lib/ticket.php'); 
+
+echo "<h1>Tests unitaires de la classe de g&eacute;n&eacute;ration de tickets.</h1>";
+
+
+$monTicket = new ticket();
+echo "<br/> ST : ".$monTicket->getServiceTicket();
+echo "<br/> TGT : ".$monTicket->getTicketGrantigTicket();
+?>
