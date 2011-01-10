b22342696017e0b3e673e25585615994d490c6d7
Michel Blanc <mblanc@erasme.org>
2011-01-10 12:33:47 +0100

    basic app routing
    added 'template' files
    fixed publish to ignore timestamp

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -avz . root@192.168.0.11:/var/www/cas/ --exclude .git/ --exclude publish.sh
+rsync -Iavz . root@192.168.0.11:/var/www/cas/ --exclude .git/ --exclude publish.sh
---
+++ views/error.php
@@ -0,0 +1,7 @@
+<?
+namespace View;
+
+function error($msg) {
+	echo "Oula, y'a une erreur : $msg<br/>";
+}
+
