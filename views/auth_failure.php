b0ff72a678cbd3237a7f946230e17f2ecefb5ff6
Pgl <pgl@erasme.org>
2011-01-13 18:19:10 +0100

    added auth_failure.php template

---
+++ views/auth_failure.php
@@ -0,0 +1,23 @@
+<?php
+/*******************************************************************************
+	@file : auth_failure.php 
+	
+	Template that handles Service Validation errors.
+*******************************************************************************/
+
+/**
+	viewAuthFailure : Callback that displays the error.	
+	
+	@file auth_success.php 
+	@author PGL pgl@erasme.org
+	@param $t an array of param with 'code', and 'message' keys.
+	@returns
+*/
+function viewAuthFailure($t){
+	echo "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+    <cas:authenticationFailure code=\"".$t['code']."\">
+        ".$t['message']."
+    </cas:authenticationFailure>
+</cas:serviceResponse>";
+}
+?>
