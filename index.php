4532d336727dc2d718b589e698c6fc67a19ce7b0
Pierre-Gilles Levallois <pgl@erasme.org>
2014-04-15 16:50:14 +0200

    adding sample file for saml configuration

--- index.php
+++ index.php
@@ -16,22 +16,10 @@
  * just fork off !
  *
  * @section License License
- * This fine piece of code is WTFPL licensed
+ * This fine piece of code is AGPL licensed
  * 
   @verbatim
-  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-  Version 2, December 2004
-
-  Copyright (C) 2004 Sam Hocevar
-  14 rue de Plaisance, 75014 Paris, France
-  Everyone is permitted to copy and distribute verbatim or modified
-  copies of this license document, and changing it is allowed as long
-  as the name is changed.
-
-  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-  0. You just DO WHAT THE FUCK YOU WANT TO.
+  See LICENSE File for more information
   @endverbatim
  *
  */
