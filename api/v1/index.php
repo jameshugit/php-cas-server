c58f435e1b680d274e6382a93f50276f7c224ccf
Bashar Saleh <bashar.saleh@gfi.fr>
2014-07-18 17:00:35 +0200

    add restfull api

---
+++ api/v1/index.php
@@ -0,0 +1,60 @@
+<?php
+require 'vendor/autoload.php';
+
+
+/**
+ * Step 1: Require the Slim Framework
+ *
+ * If you are not using Composer, you need to require the
+ * Slim Framework and register its PSR-0 autoloader.
+ *
+ * If you are using Composer, you can skip this step.
+ */
+/**
+ * Step 2: Instantiate a Slim application
+ *
+ * This example instantiates a Slim application using
+ * its default settings. However, you will usually configure
+ * your Slim application now by passing an associative array
+ * of setting names and values into the application constructor.
+ */
+$app = new \Slim\Slim(array(
+    'debug' => true));
+
+/**
+ * Step 3: Define the Slim application routes
+ *
+ * Here we define several Slim application routes that respond
+ * to appropriate HTTP request methods. In this example, the second
+ * argument for `Slim::get`, `Slim::post`, `Slim::put`, and `Slim::delete`
+ * is an anonymous function.
+ */
+
+// POST route
+$app->post('/tickets', function ()  use ($app) {
+    $username = $app->request()->params('username');
+    $password = $app->request()->params('password');
+    echo 'This service returns a ticket granting ticket when user name ad password or the service if ticket is provided';
+});
+
+
+$app->get('/tickets/:tgt', function($tgt){
+
+    echo 'returns the service ticket for the'.$tgt; 
+});
+
+
+// DELETE route
+$app->delete('/tickets', function () {
+    echo 'This is a DELETE  ticket route';
+});
+
+/**
+ * Step 4: Run the Slim application
+ *
+ * This method should be called last. This executes the Slim application
+ * and returns the HTTP response to the HTTP client.
+ */
+$app->run();
+
+?>
