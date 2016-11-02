4c7ba4b941a7ecdcfb2fcd2ffa4110f0b26d5865
Daniel LACROIX <dlacroix@erasme.org>
2016-11-02 11:01:12 +0100

    change the layout for the footer logos to allow non transparent logos

--- views/footer.php
+++ views/footer.php
@@ -4,10 +4,13 @@
 //------------------------------------------------------------------------------
 function getFooter(){
 	echo'
-		</center>
+				<div class="footer">
+					<img draggable="false" style="width: 40%" src="images/grandlyon-logo-blanc.svg" alt="Logo Métropole du Grand Lyon" />
+					<img draggable="false" style="width: 25%" src="images/logo-academie-blanc.svg" alt="Logo Académie de Lyon" />
+				</div>
+			</center>
 		</div>
 	</body>
 </html>';
 }
 
-?>
